3f3cca7e57d70b0d9b1aadb0a0e516117ccf5ffb
Nicolas Ramirez <ramirezcgn@gmail.com>
2021-06-26 17:18:35 -0300

    456

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  screen,
+  queryByAttribute,
+} from '@testing-library/react';
 import App from './App';
+
+const getById = queryByAttribute.bind(null, 'id');
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Super Calculadora Sumadora "Dora"/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const dom = render(<App />);
+  fireEvent.change(getById(dom.container, /numA/i), {
+    target: { value: '5' },
+  });
+  fireEvent.change(getById(dom.container, /numB/i), {
+    target: { value: '3' },
+  });
+  fireEvent.click(screen.getByText(/Sumar/i));
+  expect(getById(dom.container, /total/i).value).toBe('8');
 });
